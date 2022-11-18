8b28448863dfcaf30f0b0ce80346854a2b467e8e
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-18 22:54:50 +0300

    feat: implement theme

---
+++ src/utils/theme.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createTheme, ThemeProvider as TProvider } from '@mui/material/styles';
+
+type TProps = {
+  children: React.ReactNode;
+};
+
+const ThemeProvider: React.FC<TProps> = ({ children }) => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#fc4753',
+        dark: '#d43842',
+      },
+      secondary: {
+        main: '#f44336',
+      },
+    },
+    typography: {
+      fontFamily: 'FFMarkW05-Medium',
+    },
+  });
+
+  return <TProvider theme={theme}>{children}</TProvider>;
+};
+
+export default ThemeProvider;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bold: ['FFMarkW05-Bold', 'sans-serif'],
+        medium: ['FFMarkW05-Medium', 'sans-serif'],
+        heavy: ['FFMarkW05-heavy', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
