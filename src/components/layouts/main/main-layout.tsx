69daccc81d9c472e2a68ff17d2d201b7c237c2e0
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-18 23:24:58 +0300

    chore: add "isLogin" prop for main layout

--- src/components/layouts/main/main-layout.tsx
+++ src/components/layouts/main/main-layout.tsx
@@ -1,16 +1,17 @@
-import { BackgroundImage } from 'assets/images';
+import { HomeBG, LoginBG } from 'assets/images';
 import Image from 'next/image';
 import React from 'react';
 
 type TProps = {
   children: React.ReactNode;
   className?: string;
+  isLogin?: boolean;
 };
 
-const MainLayout: React.FC<TProps> = ({ children, ...other }) => {
+const MainLayout: React.FC<TProps> = ({ isLogin, children, ...other }) => {
   return (
     <div className="min-h-screen w-full" {...other}>
-      <Image src={BackgroundImage} alt="bg" className="absolute -z-10 h-[100vh] object-cover" />
+      <Image src={isLogin ? LoginBG : HomeBG} alt="bg" className="absolute -z-10 h-[100vh] object-cover" />
       <div>{children}</div>
     </div>
   );
