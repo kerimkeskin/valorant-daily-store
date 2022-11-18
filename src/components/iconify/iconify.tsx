2f1c456a0c2d2a82adf87cf95fcdd50d1d9129a9
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-18 22:11:24 +0300

    chore: add iconify component

---
+++ src/components/iconify/iconify.tsx
@@ -0,0 +1,12 @@
+import { Icon } from '@iconify/react';
+import { Box } from '@mui/material';
+
+interface IProps {
+  icon: string;
+  className?: string;
+  sx?: any;
+}
+
+export default function Iconify({ icon, sx, className, ...other }: IProps) {
+  return <Box component={Icon} icon={icon} sx={{ ...sx }} className={className} {...other} />;
+}
