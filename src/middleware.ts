c8752d8a6a72ff3d832f3c32dcd3b031c13254e6
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-20 22:43:03 +0300

    chore: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,17 @@ import type { NextRequest } from 'next/server';
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
+  const hasToken = req.cookies.has('user');
+
+  if (!hasToken) return NextResponse.redirect(new URL('/login', req.url));
+
   if (pathname == '/') {
     return NextResponse.redirect(new URL('/daily-store', req.url));
   }
+
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/daily-store', '/reminder'],
+};
