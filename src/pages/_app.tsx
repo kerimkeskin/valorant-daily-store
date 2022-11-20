910c9b8ca5d25c9244fcfa3977bb87efa23b5c99
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-20 18:43:50 +0300

    chore: add react toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
 import ThemeProvider from 'utils/theme';
 import MainLayout from 'components/layouts/main/main-layout';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +13,18 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider>
         <MainLayout>
           <Component {...pageProps} />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
         </MainLayout>
       </ThemeProvider>
     </Provider>
