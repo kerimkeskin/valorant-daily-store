8eaecc52da602a17b9de49697534931c1744366f
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-22 00:41:22 +0300

    fix: iconify "sx" prop type

--- src/components/iconify/iconify.tsx
+++ src/components/iconify/iconify.tsx
@@ -1,10 +1,11 @@
 import { Icon } from '@iconify/react';
 import Box from '@mui/material/Box';
+import { SxProps } from '@mui/material';
 
 interface IProps {
   icon: string;
   className?: string;
-  sx?: any;
+  sx?: SxProps;
 }
 
 export default function Iconify({ icon, sx, className, ...other }: IProps) {
