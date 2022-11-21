30e16cfca96f04f8fd6f78aa25f3de86a52aa308
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-22 00:28:18 +0300

    feat: implement reminder service

--- src/components/reminder/reminder-card.tsx
+++ src/components/reminder/reminder-card.tsx
@@ -3,15 +3,17 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SkinDropdown from './skin-dropdown';
 import Iconify from 'components/iconify/iconify';
+import { useLazyReminderQuery } from 'services/reminder.service';
+import { toast } from 'react-toastify';
 
 type IState = {
   email: string;
-  displayName: IDisplayName;
+  selectedItem: ISelectedItem;
 };
 
-interface IDisplayName {
-  uuid?: string;
-  displayName?: string;
+interface ISelectedItem {
+  uuid: string;
+  displayName: string;
   levelItem?: string;
   displayIcon?: string;
   streamedVideo?: string;
@@ -21,18 +23,43 @@ interface IDisplayName {
 const ReminderCard: React.FC = () => {
   const [state, setState] = useState<IState>({
     email: '',
-    displayName: {},
+    selectedItem: {
+      uuid: '',
+      displayName: '',
+      levelItem: '',
+      displayIcon: '',
+      streamedVideo: '',
+      assetPath: '',
+    },
   });
+
+  const [triggerReminder] = useLazyReminderQuery();
 
   const onChangeInput = useCallback((e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     setState((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   }, []);
 
   const onChangeDropdown = useCallback((e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>, value: any) => {
-    setState((prev) => ({ ...prev, displayName: value }));
+    setState((prev) => ({ ...prev, selectedItem: value }));
   }, []);
 
-  const onSubmit = useCallback(() => {}, []);
+  const onSubmit = useCallback(() => {
+    const { email, selectedItem } = state;
+
+    if (!email || !selectedItem.uuid) return;
+
+    const params = {
+      email,
+      skin_id: selectedItem.uuid,
+    };
+
+    triggerReminder({ ...params })
+      .unwrap()
+      .then((res) => {
+        toast.success(res.message);
+      })
+      .catch((err) => toast.error(err?.message));
+  }, [state, triggerReminder]);
 
   return (
     <div className="mt-10 flex justify-center p-3">
@@ -41,6 +68,9 @@ const ReminderCard: React.FC = () => {
           <span className="text-3xl font-bold">REMINDER</span>
           <Iconify className="text-3xl" icon="material-symbols:notification-add" />
         </div>
+        <span className="font-bold text-gray-500">
+          You can receive an e-mail notification when the skin you choose comes to your daily store.
+        </span>
         <TextField
           size="medium"
           fullWidth
@@ -52,7 +82,7 @@ const ReminderCard: React.FC = () => {
           value={state.email}
           onChange={onChangeInput}
         />
-        <SkinDropdown onChange={onChangeDropdown} value={state.displayName} placeholder="Select Skin" />
+        <SkinDropdown onChange={onChangeDropdown} value={state.selectedItem} placeholder="Select Skin" key={state.selectedItem.uuid} />
         <Button
           endIcon={<Iconify icon="fluent-mdl2:mail-reminder" />}
           variant="contained"
