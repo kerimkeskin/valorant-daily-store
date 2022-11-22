a9284a034ea9bbabfe11918048fa1917eca110a9
Kerim Keskin <abdulkerim.keskin@cmvteknoloji.com>
2022-11-22 23:35:22 +0300

    chore: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,5 +16,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/daily-store', '/reminder', '/'],
+  matcher: ['/daily-store', 'daily-email', '/reminder', '/'],
 };
